d5d2995d4764ccc439cdc32ebb0efed1f64d18e4
Maria Nunes <maria.nunes@aicos.fraunhofer.pt>
2022-02-18 15:08:13 +0000

    Rectify setup.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     download_url="https://github.com/fraunhoferportugal/tssearch/archive/refs/tags/v0.1.0.tar.gz",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages("tssearch"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
