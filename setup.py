12ae5e24b974f13874f0d105c39c5cd63472399f
Duarte Folgado <duarte.folgado@fraunhofer.pt>
2024-06-10 09:23:06 +0100

    Quick improvement on the setup.py.
    Created separate requirements for main, docs and dev. Added pre-commit.

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
+from pathlib import Path
+
 import setuptools
 
+ROOT = Path(__file__).parent
 
-with open("README.md", "r") as fh:
+with open("README.md") as fh:
     long_description = fh.read()
+
+
+def find_requirements(filename):
+    with (ROOT / "requirements" / filename).open() as f:
+        return [s for s in [line.strip(" \n") for line in f] if not s.startswith("#") and s != ""]
+
+
+install_requirements = find_requirements("requirements.txt")
+docs_requirements = find_requirements("requirements-docs.txt")
 
 setuptools.setup(
     name="tssearch",
@@ -12,12 +24,15 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     download_url="https://github.com/fraunhoferportugal/tssearch/archive/refs/tags/v0.1.3.tar.gz",
-    package_data={'tssearch': ['distances/distances.json', 'examples/ecg.pickle']},
+    package_data={"tssearch": ["distances/distances.json", "examples/ecg.pickle"]},
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['scipy', 'pandas', 'matplotlib', 'numpy'],
+    install_requires=install_requirements,
+    extras_require={
+        "docs": docs_requirements,
+    },
 )
